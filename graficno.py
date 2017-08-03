d80b9015b0732f1c539e932e705c1d76c71def76
Ema Cesek <ema.cesek@gmail.com>
2017-08-03 22:43:44 +0200

    Dela iskanje in prikaz

--- graficno.py
+++ graficno.py
@@ -1,61 +1,79 @@
 from iscem_idejo import Ideje
+from tkinter import *
 import tkinter as tk
 import random
+import os
 
 class Izgled:
     def __init__(self):
         self.i = Ideje()
-        self.zacetno_okno()    
+        self.zacetno_okno()
 
     def zacetno_okno(self):
-        okno1 = tk.Tk()
-        oznaka = tk.Label(okno1, text='Kam na izlet?'.upper())
+        prvo_okno = tk.Tk()
+        oznaka = tk.Label(prvo_okno, text='Kam na izlet?'.upper())
         oznaka.grid(row=0)
-        gumb1 = tk.Button(okno1, text='Iščem idejo', command = self.iscemo_idejo)
+        gumb1 = tk.Button(prvo_okno, text='Iščem idejo', command = self.iscemo_idejo)
         gumb1.grid(row=1, column=2)
-        gumb2 = tk.Button(okno1, text='Dodal/-a bi nov kraj')
+        gumb2 = tk.Button(prvo_okno, text='Dodal/-a bi nov kraj')
         gumb2.grid(row=2, column=2)
 
     def iscemo_idejo(self):
-        okno2 = tk.Tk()
-        oznaka = tk.Label(okno2, text='Kako bi želel začeti iskanje?')
+        self.i.regija = None
+        self.i.izbira = None
+        self.i.seznam_oznak = []
+        self.ustrezni_cilji = None
+        self.i.primerni_cilji = []
+        
+        self.odlocitveno_okno = tk.Tk()
+        oznaka = tk.Label(self.odlocitveno_okno, text='Kako bi želel začeti iskanje?')
         oznaka.pack()
-        gumb21 = tk.Button(okno2, text='Glede na regijo in oznake', command = self.iskanje_po_regijah_in_oznakah)
-        gumb21.pack()
-        gumb22 = tk.Button(okno2, text='Preseneti me z naključno izbiro', command = self.nakljucno_iskanje)
-        gumb22.pack()
-        gumb23 = tk.Button(okno2, text='Priljubljeno')
-        gumb23.config(state='disabled')
-        gumb23.pack()
+        gumb1 = tk.Button(self.odlocitveno_okno, text='Glede na regijo in oznake', command = self.iskanje_po_regijah_in_oznakah)
+        gumb1.pack()
+        gumb2 = tk.Button(self.odlocitveno_okno, text='Preseneti me z naključno izbiro', command = self.nakljucno_iskanje)
+        gumb2.pack()
+
+        gumb3 = tk.Button(self.odlocitveno_okno, text='Priljubljeno', command=self.priljubljeno_iskanje)
+        if self.i.stevilo_iskanih_oznak < 20:
+            gumb3.config(state='disabled')
+        gumb3.pack()
 
     def nakljucno_iskanje(self):
         self.i.nakljucna_izbira()
-        # odpremo novo okno in jo prikažemo, zraven lohk že kr ta kraj prikaže
-        okno = tk.Tk()
-        nalepka_o_izbiri = tk.Label(okno, text=self.i.izbira)
+        self.odlocitveno_okno.destroy()
+        
+        self.okno_za_nakljucno = tk.Tk()
+        nalepka_o_izbiri = tk.Label(self.okno_za_nakljucno, text=self.i.izbira)
         nalepka_o_izbiri.pack()
+
+        prikaz = tk.Button(self.okno_za_nakljucno, text='Prikaži kraj', command=self.prikazi_kraj)
+        prikaz.pack()
+
+        znova_zacni = tk.Button(self.okno_za_nakljucno, text='Znova začnimo iskanje', command=self.zacnimo_znova)
+        znova_zacni.pack()
         
     def iskanje_po_regijah_in_oznakah(self):
+        self.odlocitveno_okno.destroy()
         self.okno_za_izbiro_regije = tk.Tk()
 
         vprasanje = tk.Label(self.okno_za_izbiro_regije, text='Imaš v mislih določeno regijo? Da/ne?')
         vprasanje.pack()
-        gumb_da = tk.Button(self.okno_za_izbiro_regije, text='DA', command = self.regijsko_iskanje)
-        gumb_da.pack()
-        gumb_ne = tk.Button(self.okno_za_izbiro_regije, text='NE', command = self.z_oznakami)
-        gumb_ne.pack()
+        self.gumb_da = tk.Button(self.okno_za_izbiro_regije, text='DA', command = self.regijsko_iskanje)
+        self.gumb_da.pack()
+        self.gumb_ne = tk.Button(self.okno_za_izbiro_regije, text='NE', command = self.z_oznakami)
+        self.gumb_ne.pack()
 
     def regijsko_iskanje(self):
-        self.okno_za_izbiro_regije.destroy()
-        okno = tk.Tk()
         for regija in self.i.mozne_regije:
             def nastavi_regijo(r=regija):
                 self.i.regija = r
                 self.i.nalozi_slovar_idej()
-                okno.destroy()
+                self.okno_za_izbiro_regije.destroy()
                 self.nadaljuj_iskanje()
-            gumb = tk.Button(okno, text=regija, command=nastavi_regijo)
+            gumb = tk.Button(self.okno_za_izbiro_regije, text=regija, command=nastavi_regijo)
             gumb.pack()
+            self.gumb_da.config(state='disabled')
+            self.gumb_ne.config(state='disabled')
 
     def nadaljuj_iskanje(self):
         self.okno_za_nadaljne_iskanje = tk.Tk()
@@ -64,45 +82,119 @@ class Izgled:
         vprasanje.pack()
         gumb_da = tk.Button(self.okno_za_nadaljne_iskanje, text='DA', command = self.z_oznakami)
         gumb_da.pack()
-        gumb_ne = tk.Button(self.okno_za_nadaljne_iskanje, text='NE', command = self.predstavi)
+        gumb_ne = tk.Button(self.okno_za_nadaljne_iskanje, text='NE', command = self.predstavi_za_regijo)
         gumb_ne.pack()
 
-    def predstavi(self):
-        print(self.i.regija)
-        for kraj in self.i.slovar.keys():
-            if self.i.slovar[kraj][0] == self.i.regija:
-                self.i.primerni_cilji.append(kraj)
-        print('Ideje: {}'.format(self.i.primerni_cilji))
+    def predstavi_za_regijo(self):
         self.okno_za_nadaljne_iskanje.destroy()
-        okno = tk.Tk()
-        ustrezni_cilji = tk.Label(okno, text ='' )
+        self.i.poznamo_regijo()
+
+        self.okno_z_oznakami = tk.Tk()
+        self.prikaz_izbrane_oznake_in_krajev()
+        self.pocisti.config(state='disabled')
 
     def z_oznakami(self):
-        self.okno_za_nadaljne_iskanje.destroy()
-        okno = tk.Tk()
-        povemo = tk.Label(okno, text='Torej nadaljujemo iskanje glede na oznake/ ključne besede in iščemo kraje iz cele Slovenije.')
-        povemo.pack()
+        if self.i.stanje == None:
+            if self.i.regija != None:
+                self.okno_za_nadaljne_iskanje.destroy()
+            elif self.i.regija == None:
+                self.okno_za_izbiro_regije.destroy()
+        
+        self.okno_z_oznakami = tk.Tk()
+        povemo = tk.Label(self.okno_z_oznakami, text='Torej nadaljujemo iskanje glede na oznake/ ključne besede in iščemo kraje iz cele Slovenije.')
+        povemo.grid(column=0)
         for oznaka in self.i.mozne_oznake:
             def nastavi_oznako(o=oznaka):
                 self.i.oznaka = o
-            gumb = tk.Button(okno, text=oznaka, command=nastavi_oznako)
-            gumb.pack()
-        self.i.vprasaj_po_oznakah()
+                self.i.zapis_iskanih_oznak_v_dat()
+                self.i.seznam_izbranih_oznak()
+                if self.ustrezni_cilji != None:
+                    self.ustrezni_cilji.destroy()
+                self.prikaz_izbrane_oznake_in_krajev()
+            gumb = tk.Button(self.okno_z_oznakami, text=oznaka, command=nastavi_oznako)
+            gumb.grid(column=0)
+
+            
+    def prikaz_izbrane_oznake_in_krajev(self):
+        if self.i.seznam_oznak != []:
+            izbrane_oznake = tk.Label(self.okno_z_oznakami, text='Izbrane oznake: {}'.format(self.i.seznam_oznak))
+            izbrane_oznake.grid(row=2, column=1)
+            self.i.poznamo_oznake()
+
+        if self.i.regija != None:
+            izbrana_regija = tk.Label(self.okno_z_oznakami, text='Izbrana regija: {}'.format(self.i.regija))
+            izbrana_regija.grid(row=3, column=1)
+    
+        self.ustrezni_cilji = tk.Label(self.okno_z_oznakami, text='Ideje: {}'.format(self.i.primerni_cilji))
+        self.ustrezni_cilji.grid(row=4, column=1)
+        self.i.primerni_cilji = []
+            
+        navodilo = tk.Label(self.okno_z_oznakami, text = 'V vnosno polje vpišite ime kraja, ki Vas zanima.')
+        navodilo.grid(row=6, column=1)
+
+        self.vnosno_polje = tk.Entry(self.okno_z_oznakami)
+        self.vnosno_polje.grid(row=7, column=1)
+
+        prikaz = tk.Button(self.okno_z_oznakami, text='Prikaži kraj', command=self.prikazi_kraj)
+        prikaz.grid(row=8, column=1)
+
+        self.pocisti = tk.Button(self.okno_z_oznakami, text='Počisti oznake', command=self.pocisti_oznake)
+        self.pocisti.grid(row=10, column=1)
+        if self.i.seznam_oznak == []:
+            self.pocisti.config(state='disabled')
+
+        znova_zacni = tk.Button(self.okno_z_oznakami, text='Znova začnimo iskanje', command=self.zacnimo_znova)
+        znova_zacni.grid(row=11, column=1)
+
+    def pocisti_oznake(self):
+        self.i.pocisti_oznake()
+        self.okno_z_oznakami.destroy()
+        self.ustrezni_cilji = None
+        self.z_oznakami()
+
+    def zacnimo_znova(self):
+        if self.i.seznam_oznak != [] or self.i.regija != None:
+            self.okno_z_oznakami.destroy()
+        else:
+            self.okno_za_nakljucno.destroy()
+        self.iscemo_idejo()
+
+    def priljubljeno_iskanje(self):
+        self.odlocitveno_okno.destroy()
+        self.i.najveckrat_iskane()
+        self.okno_z_oznakami = tk.Tk()
+        self.prikaz_izbrane_oznake_in_krajev()
+        self.pocisti.config(state='disabled')
+
+    def prikazi_kraj(self):
+        if self.i.izbira == None:
+            self.i.izbira = self.vnosno_polje.get()
+        self.i.preveri_regijo()
         
+        self.okno_za_prikaz = tk.Toplevel()
+
+        text1 = tk.Text(self.okno_za_prikaz)
+        photo= PhotoImage(file='Bohinj.gif')
+        text1.insert(END, self.i.izbira)
+        text1.image_create(END, image=photo)
+        text1.pack(side=LEFT)
+
+        text2 = tk.Text(self.okno_za_prikaz)
+        text2.insert(END, self.i.regija + '\n')
+        text2.insert(END, self.i.oznake_kraja(self.i.izbira))
+        text2.insert(END, '\nOpis kraja')
+        text2.pack(side=LEFT)
+
+        self.okno_za_prikaz.mainloop()
+        
+
+
+
 
 Izgled()
 
 
 
-#lohk tud .grid(row= , column= )
-#vnosno_polje.get() kar je v tk.Entry()
-#button.config(state='disabled' or 'normal')
-
-##
-##
-##okno3 = tk.Tk()
-##vnos = tk.Entry(okno3)
-##vnos.pack()
 
 #http://www.python-course.eu/tkinter_text_widget.php
 
--- iscem_idejo.py
+++ iscem_idejo.py
@@ -1,4 +1,7 @@
 import random
+import os.path
+import heapq
+
 class Ideje:
     def __init__(self):
         self.regija = None
@@ -8,8 +11,10 @@ class Ideje:
         self.izbira = None
         self.primerni_cilji = []
         self.mozne_oznake = self.vse_oznake()
-#        self.preberemo_seznam_iskanih_oznak()
-#        self.datoteka_z_oznakami()
+        self.seznam_oznak = []
+        self.stanje = None
+        self.cisti_zacetek()
+        self.stetje_iskanih_oznak()
         
     def nalozi_slovar_idej(self):
         if self.regija != None:
@@ -18,86 +23,17 @@ class Ideje:
                 if self.slovar[kraj][0] == self.regija:
                        nov_slovar[kraj] = self.slovar[kraj]
             self.slovar = nov_slovar
+            #slovar samo za tocno doloceno regijo
         else:
             with open('ideje.txt') as f:
                 vse_ideje = f.read()
                 posebej = vse_ideje.split(';')
                 for vse_za_kraj in posebej:
                     loceno = vse_za_kraj.split(':')
-                    seznam = loceno[1].strip().split(',')
-                    self.slovar[loceno[0].strip()] = tuple(seznam)
-                
-    def poisci_ideje(self):
-        self.primerni_cilji = []
-        self.vprasaj_po_regiji()
-        if self.regija is None:
-            self.vprasaj_po_oznakah()
-        else: 
-            self.nalozi_slovar_idej()
-            self.nadaljujemo_iskanje()
+                    seznam_atributov = loceno[1].strip().split(',')
+                    self.slovar[loceno[0].strip()] = seznam_atributov
+        #slovar krajev, atributi kot seznam
 
-    def vprasaj_po_regiji(self):
-        self.odlocitev = input('Imaš v mislih določeno regijo? Da/Ne')
-        if self.odlocitev == 'Da':
-            # izbere gumb z eno od regij: Gorenjska, Dolenjska, Primorska, Koroška,
-            # Štajerska, Notranjska, Prekmurje
-            self.regija = input() #dobim s pritiskom gumba
-            for kraj in self.slovar.keys():
-                if self.slovar[kraj][0] == self.regija:
-                    self.primerni_cilji.append(kraj)
-            print('Ideje: {}'.format(self.primerni_cilji))
-           
-        elif self.odlocitev == 'Ne':
-            print('Iščemo torej kraje iz cele Slovenije.')
-
-        else:
-            print('Odgovoriš lahko samo z Da ali Ne.')
-            self.vprasaj_po_regiji()
-
-
-    def nadaljujemo_iskanje(self):
-        print('Želite nadaljevati iskanje glede na oznake/ ključne besede?')
-        nadaljujemo = input('Da/Ne')
-        if nadaljujemo == 'Da':
-            self.vprasaj_po_oznakah()
-        else:
-            self.izbira = self.primerni_cilji
-            print('Ideje: {}'.format(self.izbira))
-
-    def vprasaj_po_oznakah(self):
-##      self.oznaka = input('Katero kategorijo oz. katero ključno besedo naj iščemo?')
-        nova_izbira = []
-        for kraj in self.slovar.keys():
-            if self.oznaka in self.oznake_kraja(kraj):
-                if kraj not in nova_izbira:
-                    nova_izbira.append(kraj)
-                    
-        if self.izbira != None:
-            izbira = list(set(nova_izbira) & set(self.izbira))
-            self.izbira = izbira
-            print('Na podlagi oznak: {}'.format(self.izbira))
-            if self.izbira == []:
-                print('Vašim kriterijem ne ustreza noben vnešen kraj.')
-                print('Poskusite znova z drugačnimi.')
-                self.izbira = None
-                self.vprasaj_po_oznakah()
-            self.dodamo_vec_oznak()
-        else:
-            self.izbira = nova_izbira
-            print('Na podlagi oznak: {}'.format(self.izbira))
-            self.dodamo_vec_oznak()
-
-
-    def dodamo_vec_oznak(self):
-        dodamo_oznako = input('Razširimo iskanje s še kakšno kategorijo? Da/Ne')
-        if dodamo_oznako == 'Da':
-            self.vprasaj_po_oznakah()
-        elif dodamo_oznako == 'Ne':
-            print('Na podlagi oznak: {}'.format(self.izbira))
-        else:
-            print('Možna odgovora le Da ali Ne.')
-            self.vprasaj_po_oznakah()
-      
     def vse_oznake(self):
         '''Seznam vseh oznak brez ponavljanja'''
         vse_oznake = []
@@ -110,12 +46,12 @@ class Ideje:
 
     def oznake_kraja(self, kraj):
         '''Seznam oznak enega kraja'''
-        return list(self.slovar[kraj][1:])
-
-
+        return self.slovar[kraj][1:]
+    
+#odlocimo se za nakljucno izbiro
     def nakljucna_izbira(self):
         self.izbira = random.choice(self.nastejemo_vse_kraje())
-            
+       
     def nastejemo_vse_kraje(self):
         '''Seznam krajev'''
         seznam_krajev = []
@@ -123,96 +59,77 @@ class Ideje:
             seznam_krajev.append(kraj)
         return seznam_krajev
 
-    def datoteka_z_oznakami(self):
-        if self.iskane_oznake == []:
+#dolocimo samo regijo
+    def poznamo_regijo(self):
+        for kraj in self.slovar.keys():
+            self.primerni_cilji.append(kraj)
+
+#dolocimo oznako
+    def seznam_izbranih_oznak(self):
+        if self.oznaka not in self.seznam_oznak:
+            self.seznam_oznak.append(self.oznaka)
+
+    def poznamo_oznake(self):
+        for kraj in self.slovar.keys():
+            vsota = 0
+            for oznaka in self.seznam_oznak:
+                if oznaka in self.oznake_kraja(kraj):
+                    vsota += 0
+                else:
+                    vsota += 1
+            if vsota == 0:
+                if kraj not in self.primerni_cilji:
+                    self.primerni_cilji.append(kraj)
+
+    def pocisti_oznake(self):
+        self.seznam_oznak = []
+        self.primerni_cilji = []
+        self.stanje = 'pocisti'
+                
+#za priljubljeno trikrat najveckrat iskane oznake
+    def cisti_zacetek(self):
+        if os.path.exists('iskane_oznake.txt'):
+            self.stetje_iskanih_oznak()
+        else:
             with open('iskane_oznake.txt', 'w') as f:
-                None
+                print(' ', file=f)
 
-    def osvezimo_seznam_iskanih_oznak(self):
+    def stetje_iskanih_oznak(self):
+        with open('iskane_oznake.txt') as f:            
+            seznam_oznak = f.read().strip().split(',')
+            self.stevilo_iskanih_oznak = len(seznam_oznak) 
+        
+    def zapis_iskanih_oznak_v_dat(self):
+        self.stevilo_iskanih_oznak += 1
         with open('iskane_oznake.txt', 'a') as f:
-            if self.iskane_oznake == []:
-                print('{}'.format(self.oznaka), file = f)
-            else:
-                print(',{}'.format(self.oznaka), file = f)
+                print(self.oznaka + ',', file=f)
 
-##    def preberemo_seznam_iskanih_oznak(self):
-##        with open('iskane_oznake.txt') as f:
-##            if f.read() == '':
-##                self.iskane_oznake = []
-##            else:
-##                self.iskane_oznake = []
-##                seznam_oznak = f.read().split(',')
-##                for beseda in seznam_oznak:
-##                    self.iskane_oznake.append(beseda.strip())
+    def najveckrat_iskane(self):
+        with open('iskane_oznake.txt') as f:
+            slovar_pojavitev = {}
+            seznam_oznak = f.read().strip().split(',')
+            for oznaka in seznam_oznak:
+                oznaka = oznaka.strip()
+                if oznaka in slovar_pojavitev.keys():
+                    slovar_pojavitev[oznaka] += 1
+                else:
+                    slovar_pojavitev[oznaka] = 1
+            self.seznam_oznak = heapq.nlargest(3, slovar_pojavitev, key=slovar_pojavitev.get)
 
-    def priljubljeno(self):
-        if len(self.iskane_oznake) > 10:
-            brez_ponovitev = []
-            for oznaka in self.iskane_oznake:
-                if oznaka not in brez_ponovitev:
-                    brez_ponovitev.append(oznaka)
-            pari = []
-            for oznaka in brez_ponovitev:
-                kolikokrat = self.iskane_oznake.count(oznaka)
-                pari.append((-kolikokrat, oznaka))
-                pari.sort()
-            self.oznaka = pari[0][1]
-            self.izbira = []
-            for kraj in self.slovar.keys():
-                if self.oznaka in self.oznake_kraja(kraj):
-                    self.izbira.append(kraj)
+#za prikaz
+    def preveri_regijo(self):
+        if self.regija == None:
+            self.regija = self.slovar[self.izbira][0]
 
-def dodamo_kraj():
-    with open('ideje.txt', 'a') as dat:
-        ime = input('Ime kraja:') #preveri naj če že obstaja kraj v slovarju
-        regija = input('Regija tvojega kraja?') #gumbki
-        oznake = input() #gumbki
-        print(';', file = dat)
-        print('{}:{},{}'.format(ime, regija, oznake), file = dat)
-
-class Kraj:
-    def __init__(self, ime):
-        self.ime = ime
-        self.vsebina_v_slovarju()
-        self.regija = self.vsebina[0]
-        self.oznake = self.vsebina[1:]
-        self.opis_kraja()
-
-    def vsebina_v_slovarju(self):
-        with open('ideje.txt') as f:
-            vse_ideje = f.read()
-            posebej = vse_ideje.split(';')
-            for vse_za_kraj in posebej:
-                if self.ime in vse_za_kraj:
-                    loceno = vse_za_kraj.split(':')
-                    self.vsebina = tuple(loceno[1].split(','))
-
-    def opis_kraja(self):
-        ime_datoteke = self.ime + '.txt'
-        with open(ime_datoteke) as f:
-            self.opis = f.read()
+        
 
 
-def dodati_opis(kraj):
-    ime_datoteke = kraj + 'txt'
-    with open(ime_datoteke, 'w') as f:
-        besedilo = input('Opis:')
-        print(besedilo, file = f)
 
-def pocisti_iskanja():
-    pass
-           
-#za nov kraj samo da se vpiše v datoteko
-#za prikaz kraja bo on vpisal Entry
-#za vsakega ime, regija, oznake, prebran opis, prebrani komentarji, ogleda sliko
-#kraj bo vnos.get ker to vrne niz
 
-#gumb počisti iskanje da začne od začetka, lahko gumb ki pove koliko krajev v bazi
-# spreminjam slovar samo lokalno: del ideje['Ljubljana']
-#za sliko:import os
-#         os.system('start slika.jpeg')
 
-##i = Ideje()
-##i.poisci_ideje()
 
+
+
+          
+#za vsakega ime, regija, oznake, prebran opis, ogleda sliko
 
